ced7a8dc6b028bb306e1759d9d29f7a3fa37cecd
AndreyCorpse28090 <280990corpse@gmail.com>
2024-06-25 20:12:32 +0300

    Добавьте файлы проекта.

---
+++ ChemicalElements.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entity
+{
+    [Table("ChemicalElements_t")]
+    internal class ChemicalElements
+    {
+        [Column("id")]
+        public int id {  get; set; }
+
+        [Column("FullName_f")]
+        public string fullName { get; set; }
+
+        [Column("Mass_f")]
+        public float? mass { get; set; }
+
+        [Column("NamedAfter_f")]
+        public string namedAfter {  get; set; }
+
+        public ChemicalElements() { }
+
+        public override string ToString()
+        {
+            return $"ID: {id}, Full Name: {fullName}, Mass: {mass?.ToString() ?? "N/A"}, Named After: {namedAfter}";
+        }
+    }
+}
